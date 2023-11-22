dcd0d915bea040d40ab6f7072255c2a5b4ea5341
LizToDataWiz <149129200+LizToDataWiz@users.noreply.github.com>
2023-11-21 21:37:00 -0500

    Update jugalbot_liezyl_traveltide_takehome.sql

--- jugalbot_liezyl_traveltide_takehome.sql
+++ jugalbot_liezyl_traveltide_takehome.sql
@@ -1,3 +1,16 @@
+/*
+Project Details:
+
+e-booking startup TravelTide is a hot new player in the online travel industry. 
+It has experienced steady growth since it was founded at the tail end of the covid pandemic (2021-04) on the strength 
+of its data aggregation and search technology.
+
+The TravelTide team has recently shifted focus from aggressively acquiring new customers to better serving their existing customers. 
+In order to achieve better service, the team recognizes that it is important to understand customer demographics and behavior.
+
+The task is to help the TravelTide team develop an understanding of their customers and give recommendations.
+*/
+
 -- 1.a Which cross-section of age and gender travels the most?
 
 WITH user_summary AS (
